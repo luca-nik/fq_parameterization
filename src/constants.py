4777b4d3a2462d4c2daad7136e4e39e61b18ab65
luca.nicoli <luca.nicoli@sns.it>
2023-05-03 16:30:25 +0200

    Constants, get the number of connnections

--- src/constants.py
+++ src/constants.py
@@ -2,11 +2,13 @@ def number_connections():
     """A function which returns a dictionary."""
     """The values indicate the threshold for the connectrivity"""
     connections = {
-    "H" :   1        ,
-    "C" :   3        ,
-    "N" :   3        ,
-    "O" :   2        ,
-    "Cl":   4        ,
+    "H"  :   1        ,
+    "HW" :   1        ,
+    "C"  :   3        ,
+    "N"  :   3        ,
+    "O"  :   2        ,
+    "OW" :   2        ,
+    "Cl" :   4        ,
     }
 
     return connections
