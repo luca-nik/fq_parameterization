ae7bfff0b87911da75da09a3abd426fa34151725
Luca Nicoli <luca.nicoli@sns.it>
2023-06-13 18:04:30 +0200

    Added the polar input preparation in qm_interface

--- src/qm_interface.py
+++ src/qm_interface.py
@@ -1,42 +1,45 @@
 from classes import molecule_class
 from classes import dipoles_class
-import constants
+from classes import cluster_class
 import numpy as np
 import sys
 import os
 #
-def create_et_EE_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '', which_dipoles = [],\
-                  et_output_file_name = '', target_directory =  './', computation_name = '', single_charge = [False, 0.0]):
+def create_et_EE_inp(et_seed_file = '', QMmolecule = '', EEdipoles = '', which_dipoles = [],\
+                     et_output_file_name = '', target_directory =  './', computation_name = '',\
+                     dipoles_distance = 0.5, single_charge = [False, 0.0]):
     #
     """Procedure to generate a .inp file (input for eT calculations)
        starting from a seed.inp file
        The seed.inp file, which is et_seed_file has everything set up except for:
        1) Name of the computation;
        2) Target QM molecule;
-       3) Static dipole embedding the QM molecule; 
+       3) Static dipole embedding of the QM molecule; 
   
        The procedure is built in such a way that the user provides:
-       1) et_seed_file.inp 
-       2) QMmolecule_file.xyz (target QM molecule xyz file)
-       3) EEdipoles_file.dip  (static dipolar embedding.dip file)
-       4) et_output_file_name.inp (saved in target_directory)
-       5) target_directory aforementioned (skip if you already have the path in the name of the file)
-       6) which_dipoles (a list of dipoles you will include in the QM/EE calculation, default is all)
-       7) computation_name (an ID inserted in the .inp file, useful for the user)
-
+         1) et_seed_file.inp        : string (This is a file where we read all the QM computation details)
+         2) QMmolecule              : molecule class (Target QM molecule)
+         3) EEdipoles               : dipoles_class  (Electrostatic embeddign dipoles)
+         4) et_output_file_name.inp : string (output .inp file name, saved in target_directory)
+         5) target_directory        : string (where you save the .inp files,
+                                              skip if you already have the path in the name of the file)
+         6) which_dipoles           : list of int (list of the indexes of the dipoles to put in the 
+                                                   QM/EE calculation, default is all)
+         7) computation_name        : string  (an ID inserted in the .inp file, useful for the user)
+         8) dipoles_distance        : float (distance of each charge of the dipole from the dipole
+                                             center of mass)
 
        WARNING: all the files need to be provided wih the proper path to fetch them!
-       WARNING: the .dip files contain information about the center of mass of the dipole
     """
     #
-    distance = constants.dipoles_distance() #distance of the EE dipoles charges from their CM 
+    distance = dipoles_distance
     #
     # Sanity checks
     #
     if type(which_dipoles) != list:
         which_dipoles = [which_dipoles]
     #
-    et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule_file, EEdipoles_file, \
+    et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule, EEdipoles, \
                                    et_output_file_name, target_directory,     \
                                    computation_name, which_dipoles)
     #
@@ -47,12 +50,6 @@ def create_et_EE_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '
     #
     with open(et_seed_file, 'r') as seed:
         seed_lines = seed.readlines()
-    #
-    QMmolecule = molecule_class.molecule()
-    QMmolecule.initialize_from_xyz(QMmolecule_file)
-    #
-    EEdipoles = dipoles_class.dipoles()
-    EEdipoles.initialize_from_dip(EEdipoles_file)
     #
     # Sanity checks on the initialized dipoles
     #
@@ -79,7 +76,7 @@ def create_et_EE_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '
         for line in seed_lines:
             #
             if ('name:' in line):
-                et_file.write('   name: coso\n')
+                et_file.write('   name: ' + computation_name + ' \n')
             #
             elif ('charge:' in line):
                 et_file.write('   charge: ' + str(int(QMmolecule.charge)) + '\n')
@@ -87,6 +84,9 @@ def create_et_EE_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '
             elif ('geometry' in line and 'end geometry' not in line):
                 et_file.write('geometry \n')
                 et_file.write('basis: '+ basis.strip() + '\n')
+                #
+                # QM molecule
+                #
                 for i, sym in enumerate(QMmolecule.atomtypes):
                     et_file.write(sym.rjust(2) + '  ' + \
                                   '{:5.5f}'.format(QMmolecule.coords[i][0]).rjust(10)+ '  ' + \
@@ -143,7 +143,7 @@ def create_et_EE_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '
 #
 #
 #
-def et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule_file, EEdipoles_file, \
+def et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule, EEdipoles, \
                                    et_output_file_name, target_directory,     \
                                    computation_name, which_dipoles):
     #
@@ -155,12 +155,12 @@ def et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule_file, EEdipoles_f
         print('ERROR: create_et_EE_inp called with ununderstandable et_seed_file')
         sys.exit()
     #
-    if (type(QMmolecule_file) != str):
-        print('ERROR: create_et_EE_inp called with ununderstandable QMmolecule_file')
+    if (not isinstance(QMmolecule,molecule_class.molecule)):
+        print('ERROR: create_et_EE_inp called without proper QM molecule object')
         sys.exit()
     #
-    if (type(EEdipoles_file) != str):
-        print('ERROR: create_et_EE_inp called with ununderstandable EEdipoles_file')
+    if (not isinstance(EEdipoles,dipoles_class.dipoles)):
+        print('ERROR: create_et_EE_inp called without proper EE dipoles object')
         sys.exit()
     #
     if (type(et_output_file_name) != str):
@@ -265,118 +265,67 @@ def adf_read_polar(adf_out_file = '', which = 'isotropic'):
 #
 #
 #
-#def create_adf_polar_inp(et_seed_file = '', QMmolecule_file = '', EEdipoles_file = '', which_dipoles = [],\
-#                  et_output_file_name = '', target_directory =  './', computation_name = '', single_charge = [False, 0.0]):
-#    #
-#    #
-#    distance = constants.dipoles_distance() #distance of the EE dipoles charges from their CM 
-#    #
-#    # Sanity checks
-#    #
-#    if type(which_dipoles) != list:
-#        which_dipoles = [which_dipoles]
-#    #
-#    et_create_et_EE_inp_sanity_checks(et_seed_file, QMmolecule_file, EEdipoles_file, \
-#                                   et_output_file_name, target_directory,     \
-#                                   computation_name, which_dipoles)
-#    #
-#    if target_directory[-1] != '/':
-#        target_directory +='/'
-#    #
-#    # Fetch the files
-#    #
-#    with open(et_seed_file, 'r') as seed:
-#        seed_lines = seed.readlines()
-#    #
-#    QMmolecule = molecule_class.molecule()
-#    QMmolecule.initialize_from_xyz(QMmolecule_file)
-#    #
-#    EEdipoles = dipoles_class.dipoles()
-#    EEdipoles.initialize_from_dip(EEdipoles_file)
-#    #
-#    # Sanity checks on the initialized dipoles
-#    #
-#    if (len(which_dipoles) == 0): #displace them all
-#        which_dipoles = [i for i in range(0, EEdipoles.n_dipoles)]
-#    for i in which_dipoles:
-#        if i > EEdipoles.n_dipoles:
-#            print('ERROR: creat_EE_inp called with non-existing dipole number ' + str(i)) 
-#            sys.exit()
-#    #
-#    # Initialize and work on the .inp file
-#    #
-#    with open(target_directory + et_output_file_name, 'w') as et_file:
-#        #
-#        # Fetch the basis used in the computation
-#        #
-#        basis  = 'aug-cc-pvdz'
-#        for line in seed_lines:
-#            if('basis:' in line):
-#                basis = line.split(':')[1]
-#        #
-#        # Write the .inp file with the correct information
-#        #
-#        for line in seed_lines:
-#            #
-#            if ('name:' in line):
-#                et_file.write('   name: coso\n')
-#            #
-#            elif ('charge:' in line):
-#                et_file.write('   charge: ' + str(int(QMmolecule.charge)) + '\n')
-#            #
-#            elif ('geometry' in line and 'end geometry' not in line):
-#                et_file.write('geometry \n')
-#                et_file.write('basis: '+ basis.strip() + '\n')
-#                for i, sym in enumerate(QMmolecule.atomtypes):
-#                    et_file.write(sym.rjust(2) + '  ' + \
-#                                  '{:5.5f}'.format(QMmolecule.coords[i][0]).rjust(10)+ '  ' + \
-#                                  '{:5.5f}'.format(QMmolecule.coords[i][1]).rjust(10)+ '  ' + \
-#                                  '{:5.5f}'.format(QMmolecule.coords[i][2]).rjust(10) + '\n')
-#                #
-#                et_file.write('--')
-#                #
-#                # EE embedding
-#                #
-#                if single_charge[0]:
-#                    for i,dip in enumerate(which_dipoles):
-#                        #
-#                        # Charge in CM
-#                        #
-#                        et_file.write( 'H ' + ' [IMol=' + str(i+1).rjust(2) + ']  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][0]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][1]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][2]).rjust(10)+ '  ' + \
-#                                       '[q = ' + str(single_charge[1]) + '] \n' )
-#                else:
-#                    for i,dip in enumerate(which_dipoles):
-#                        # 
-#                        plus_vector  =  distance*EEdipoles.directions[dip,:]
-#                        minus_vector = -distance*EEdipoles.directions[dip,:]
-#                        plus_vector_sign  = EEdipoles.signs[dip][1]
-#                        minus_vector_sign = EEdipoles.signs[dip][0]
-#                        #
-#                        # Charge in CM - distance
-#                        #
-#                        et_file.write('\n' + 'H ' + ' [IMol=' + str(i+1).rjust(2) + ']  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][0] + minus_vector[0]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][1] + minus_vector[1]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][2] + minus_vector[2]).rjust(10)+ '  ' + \
-#                                       '[q = ' + minus_vector_sign + '1.0] \n' )
-#                        #
-#                        # Charge in CM + distance
-#                        #
-#                        et_file.write( 'H ' + ' [IMol=' + str(i+1).rjust(2) + ']  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][0] + plus_vector[0]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][1] + plus_vector[1]).rjust(10)+ '  ' + \
-#                                       '{:5.5f}'.format(EEdipoles.positions[dip][2] + plus_vector[2]).rjust(10)+ '  ' + \
-#                                       '[q = ' + plus_vector_sign + '1.0] \n' )
-#            #
-#            elif('xxx' in line):
-#                pass
-#            #
-#            elif('basis:' in line): #already done
-#                pass
-#            #
-#            else:
-#                et_file.write(line)
-#    #
+def create_adf_polar_inp(adf_seed_file = '', cluster = '', adf_output_file_name = '', target_directory =  './'):
+    #
+    # Sanity checks
+    #
+    create_adf_polar_inp_sanity_checks(adf_seed_file,cluster,adf_output_file_name,target_directory)
+    #
+    if target_directory[-1] != '/':
+        target_directory +='/'
+    #
+    # Fetch the files
+    #
+    with open(adf_seed_file, 'r') as seed:
+        seed_lines = seed.readlines()
+    #
+    # Initialize and work on the .inp file
+    #
+    with open(target_directory + adf_output_file_name, 'w') as adf_file:
+        #
+        # Write down information from seed file
+        #
+        for line in seed_lines:
+            if('  Atoms' in line):
+                adf_file.write(line)
+                for mol_indx, molecule in enumerate(cluster.molecules):
+                    for i, sym in enumerate(molecule.atomtypes):
+                        #
+                        # Take the right symbol
+                        #
+                        if sym == 'Cl':
+                            right_sym = sym
+                        else:
+                            right_sym = sym[0]
+                        #
+                        adf_file.write('    ' + right_sym.ljust(2)  + \
+                                       '{:5.5f}'.format(molecule.coords[i][0]).rjust(10)+ '  ' + \
+                                       '{:5.5f}'.format(molecule.coords[i][1]).rjust(10)+ '  ' + \
+                                       '{:5.5f}'.format(molecule.coords[i][2]).rjust(10) + '\n')
+            else:
+                adf_file.write(line)
+#
+#
+#
+def create_adf_polar_inp_sanity_checks(adf_seed_file,cluster,adf_output_file_name,target_directory):
+    #
+    """Routine for the sanity checks"""
+    #
+    # Check the types of the files
+    #
+    if (type(adf_seed_file) != str):
+        print('ERROR: create_adf_polar_inp called with ununderstandable adf_seed_file')
+        sys.exit()
+    #
+    if (not isinstance(cluster,cluster_class.cluster)):
+        print('ERROR: create_adf_polar_inp called without proper cluster object')
+        sys.exit()
+    #
+    if (type(adf_output_file_name) != str):
+        print('ERROR: create_adf_polar_inp called with ununderstandable adf_output_file_name')
+        sys.exit()
+    #
+    if (type(target_directory) != str):
+        print('ERROR: create_adf_polar_inp called with ununderstandable target_directory')
+        sys.exit()
+    #
